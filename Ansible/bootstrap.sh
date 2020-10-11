e31480d582d6e745b31c0fbe6fd8791893df90c1
Shlomi Kaduri <42743238+shlomikaduri@users.noreply.github.com>
2020-10-11 17:31:52 +0300

    EX14 HW - Ansible

---
+++ Ansible/bootstrap.sh
@@ -0,0 +1,3 @@
+cp /etc/ansible/sample/handlers/main.yml /etc/ansible/roles/nginx/handlers/main.yml
+cp /etc/ansible/sample/tasks/main.yml /etc/ansible/roles/nginx/tasks/main.yml
+cp /etc/ansible/sample/server.yml /etc/ansible/server.yml
